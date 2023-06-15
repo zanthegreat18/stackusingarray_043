9a63e3c36bf44246172bc072aa46e8df22a7f36f
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-06-15 20:17:53 +0700

    commit 9

--- stackarray/stackarray.cpp
+++ stackarray/stackarray.cpp
@@ -54,3 +54,27 @@ public:
 		}
 	}
 };
+
+
+int main() {
+	Program s;
+
+	while (true) {
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter your choice: ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nEnter an element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
