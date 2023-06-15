21d335890bf34eeb044dff3773eee9b1d4741dbc
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-06-15 20:16:48 +0700

    commit 5

--- stackarray/stackarray.cpp
+++ stackarray/stackarray.cpp
@@ -27,3 +27,14 @@ public:
 
 		return element;
 	}
+
+
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop." << endl;
+			return;
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
